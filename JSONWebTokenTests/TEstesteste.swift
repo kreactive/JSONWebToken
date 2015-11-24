c11d927da88b0b1c19838dc6f32c9a0d5ecf8012
Antoine Palazzolo <antoine.palazzolo@gmail.com>
2015-11-24 12:04:51 +0100

    remove unused

--- JSONWebTokenTests/TEstesteste.swift
+++ JSONWebTokenTests/TEstesteste.swift
@@ -3,13 +3,8 @@
 //  JSONWebToken
 //
 //  Created by Antoine Palazzolo on 23/11/15.
-//  Copyright © 2015 Antoine Palazzolo. All rights reserved.
 //
 
 import Foundation
 
-
-
-
-let verifier = HMACSignature(secret: "616161".dataUsingEncoding(NSUTF8StringEncoding)!, hashFunction: .SHA256)
 
