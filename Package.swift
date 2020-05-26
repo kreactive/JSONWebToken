81fb651550f5ced56a5e98622fe1af585be481c9
Guillaume Bourachot <g.bourachot@kreactive.com>
2020-05-26 10:15:17 +0200

    Addition of the SPM support

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:4.0
+
+import PackageDescription
+
+let pkg = Package(name: "JSONWebToken")
+pkg.products = [
+    .library(name: "JSONWebToken", targets: ["JSONWebToken"]),
+]
+
+let pmk: Target = .target(name: "JSONWebToken")
+pmk.path = "JSONWebToken"
+pkg.swiftLanguageVersions = [5]
+pkg.targets = [
+    pmk,
+    .testTarget(name: "JSONWebTokenTests", dependencies: ["JSONWebToken"], path: "JSONWebTokenTests"),
+]
